2813f9ed586457cfdc7458e4792cf67c86a894e5
natmrcl <natyamm10@gmail.com>
2023-03-15 10:44:55 +0700

    Add project files.

---
+++ The Cashier/Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Cashier
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string title, int quantity, double price, double subtotal, string type)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = subtotal;
+            this.type = type;
+        }
+        
+        public string getTitle() { return title; }
+        public int getQuantity() { return quantity; }
+        public double getPrice() { return price; }
+        public double getSubtotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+
+    }
+}
